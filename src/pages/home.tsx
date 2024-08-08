9af5020eb70727b62e10b6887277740ff7f33c17
brianj-incento <heuiyeong@incento.kr>
2024-08-08 17:27:40 +0900

    DIV and CSS fixed
    DIV and CSS fixed

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -16,19 +16,17 @@ const HomePage = () => {
 
   return (
     <div className="LoginContainer flex justify-center items-center h-screen bg-[#8ace00]">
-      <div className="LoginBox min-w-[350px] min-h-[350px] border-2 border-white flex justify-center items-center">
-        <div className="inline-block w-[100%] h-[100%]">
-          <div className=" flex justify-center m-10">
-            <img src="/images/incento_logo.png" alt="Incento Logo" />
-          </div>
-          <div className="p-4 flex items-center justify-center space-x-2 m-10">
-            <button id="login" className="p-2  bg-gray-300 text-auto text-sm" onClick={handleButton}>
-              로그인
-            </button>
-            <button id="signup" className="p-2  bg-gray-300 text-auto text-sm" onClick={handleButton}>
-              회원가입
-            </button>
-          </div>
+      <div className="LoginBox min-w-[380px] min-h-[380px] border border-white flex flex-col justify-center items-center">
+        <div className="m-10 flex justify-center">
+          <img src="/images/incento_logo.png" alt="Incento Logo" className="w-[180px]" />
+        </div>
+        <div className="p-4 flex items-center justify-center space-x-2 m-10">
+          <button id="login" className="p-2 bg-gray-300 text-sm" onClick={handleButton}>
+            로그인
+          </button>
+          <button id="signup" className="p-2 bg-gray-300 text-sm" onClick={handleButton}>
+            회원가입
+          </button>
         </div>
       </div>
     </div>
