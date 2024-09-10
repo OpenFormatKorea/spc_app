acc933723e0997336d3b036216972bc18cd4260f
brianj-incento <heuiyeong@incento.kr>
2024-09-10 16:50:43 +0900

    button fix for dashboard
    button fix for dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -45,10 +45,8 @@ const Dashboard: React.FC<{ apiResponse: ApiResponse; authResponse: AuthArgs }> 
     }
   };
 
-  useEffect(() => {}, [apiResponse, authResponse]);
-
   return (
-    <DashboardContainer title="대시보드" onclick={handleButton} onclickText="" buttonId="dashboard">
+    <DashboardContainer title="대시보드">
       <div className="contents-container w-full justify-center ">
         <ContentsContainer variant="dashboard">
           <CampaignList
