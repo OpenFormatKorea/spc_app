13e341ba7fbc4035683d33ea304fe121ec0453f5
brianj-incento <heuiyeong@incento.kr>
2024-09-10 11:32:47 +0900

    UX/UI change for the navbar
    UX/UI change for the navbar

--- src/components/layout/base/Navmenu.tsx
+++ src/components/layout/base/Navmenu.tsx
@@ -33,8 +33,8 @@ const Navmenu: React.FC<NavmenuProps> = ({ link, menutitle }) => {
 
   return (
     <div
-      className={`h-[55px] flex items-center text-md cursor-pointer transition-colors duration-300 text-white w-full p-4
-    ${isActive ? "bg-sky-700" : isHovered ? "lg:bg-sky-700 sm:bg-sky-500" : "lg:bg-sky-900 sm:bg-sky-700"}
+      className={`h-[55px] flex items-center text-md cursor-pointer transition-all duration-300 text-white w-full p-4
+    ${isActive ? "bg-sky-700 scale-90 rounded-xl" : isHovered ? "lg:bg-sky-700 sm:bg-sky-500 scale-90 rounded-xl" : "lg:bg-sky-900 sm:bg-sky-700 scale-100"}
   `}
       onClick={handleClick}
       onMouseEnter={handleMouseEnter}
