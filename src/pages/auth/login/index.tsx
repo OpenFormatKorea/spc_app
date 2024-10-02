38c6e103265a6fad01a2cdc0ff997e36bd06340f
brianj-incento <heuiyeong@incento.kr>
2024-10-02 12:15:30 +0900

    result.data console add
    result.data console add

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -60,8 +60,11 @@ const Login: React.FC = () => {
   const handleSubmit = async (event: React.FormEvent) => {
     if (infoCheck(loginInfo)) {
       const result = await postLogin(loginInfo);
+      console.log("result.data:: handle submit: ", result.data);
+
       if (result.success) {
         setCookie("access_standalone", result.data?.access);
+        console.log("result.data?.access:: handle submit: ", result.data?.access);
         setCookie("refresh", result.data?.refresh);
         setCookie("shop_id", result.data?.shop_id);
         router.push("/dashboard");
