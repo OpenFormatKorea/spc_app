3e2f220866207bd2593a80b207dfcfd0b63259aa
brianj-incento <heuiyeong@incento.kr>
2025-02-10 15:44:04 +0900

    delete unused code
    delete unused code

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -9,7 +9,9 @@ import {
 } from "@/lib/helper";
 import { jwtDecode } from "jwt-decode";
 
-export const getAxiosInstanceServer = async (context: GetServerSidePropsContext) => {
+export const getAxiosInstanceServer = async (
+  context: GetServerSidePropsContext,
+) => {
   try {
     const access = getAccessTokenFromCookies(context);
     const refresh = getRefreshTokenFromCookies(context);
@@ -32,53 +34,62 @@ export const getAxiosInstanceServer = async (context: GetServerSidePropsContext)
     });
 
     // Request interceptor to handle token expiration
-    axiosInstance.interceptors.request.use(async (req: InternalAxiosRequestConfig) => {
-      if (accessToken) {
-        const access_decoded: { exp: number } = jwtDecode(accessToken as string);
-        const isAccessExpired = access_decoded.exp * 1000 < new Date().getTime();
+    axiosInstance.interceptors.request.use(
+      async (req: InternalAxiosRequestConfig) => {
+        if (accessToken) {
+          const access_decoded: { exp: number } = jwtDecode(
+            accessToken as string,
+          );
+          const isAccessExpired =
+            access_decoded.exp * 1000 < new Date().getTime();
 
-        // If access token is expired, refresh it
-        if (isAccessExpired && refresh) {
-          const response = await axios.post(`${baseURL}/account/token/refresh/`, { refresh });
-          setAccessTokenToCookies(context, response.data.access);
-          setRefreshTokenToCookies(context, response.data.refresh);
-          req.headers.Authorization = `Bearer ${response.data.access}`;
-          accessToken = response.data.access;
+          // If access token is expired, refresh it
+          if (isAccessExpired && refresh) {
+            const response = await axios.post(
+              `${baseURL}/account/token/refresh/`,
+              { refresh },
+            );
+            setAccessTokenToCookies(context, response.data.access);
+            setRefreshTokenToCookies(context, response.data.refresh);
+            req.headers.Authorization = `Bearer ${response.data.access}`;
+            accessToken = response.data.access;
+          }
         }
-      }
 
-      // Check if refresh token is expired
-      if (refresh) {
-        const refresh_decoded: { exp: number } = jwtDecode(refresh as string);
-        const isRefreshExpired = refresh_decoded.exp * 1000 < new Date().getTime();
+        // Check if refresh token is expired
+        if (refresh) {
+          const refresh_decoded: { exp: number } = jwtDecode(refresh as string);
+          const isRefreshExpired =
+            refresh_decoded.exp * 1000 < new Date().getTime();
 
-        if (isRefreshExpired) {
-          deleteCookie("access_standalone", context);
-          deleteCookie("refresh_standalone", context);
+          if (isRefreshExpired) {
+            deleteCookie("access_standalone", context);
+            deleteCookie("refresh_standalone", context);
 
-          // Server-side redirection
-          if (typeof window === "undefined") {
-            context.res.writeHead(302, {
-              Location:
+            // Server-side redirection
+            if (typeof window === "undefined") {
+              context.res.writeHead(302, {
+                Location:
+                  process.env.NODE_ENV === "development"
+                    ? "http://dev-fe.standalone.incento.kr/auth/login"
+                    : "https://dev-fe.standalone.incento.kr/auth/login",
+              });
+              context.res.end();
+              return req;
+            } else {
+              window.location.replace(
                 process.env.NODE_ENV === "development"
                   ? "http://dev-fe.standalone.incento.kr/auth/login"
                   : "https://dev-fe.standalone.incento.kr/auth/login",
-            });
-            context.res.end();
-            return req;
-          } else {
-            window.location.replace(
-              process.env.NODE_ENV === "development"
-                ? "http://dev-fe.standalone.incento.kr/auth/login"
-                : "https://dev-fe.standalone.incento.kr/auth/login"
-            );
-            return req;
+              );
+              return req;
+            }
           }
         }
-      }
 
-      return req;
-    });
+        return req;
+      },
+    );
 
     // Response interceptor
     axiosInstance.interceptors.response.use((response) => {
@@ -93,7 +104,7 @@ export const getAxiosInstanceServer = async (context: GetServerSidePropsContext)
       window.location.replace(
         process.env.NODE_ENV === "development"
           ? "http://dev-fe.standalone.incento.kr/auth/login"
-          : "https://dev-fe.standalone.incento.kr/auth/login"
+          : "https://dev-fe.standalone.incento.kr/auth/login",
       );
     } else {
       context.res.writeHead(302, {
@@ -108,90 +119,3 @@ export const getAxiosInstanceServer = async (context: GetServerSidePropsContext)
     return null;
   }
 };
-
-// export const getAxiosInstanceServer = async (context: GetServerSidePropsContext) => {
-//   try {
-//     const access = getAccessTokenFromCookies(context);
-//     const refresh = getRefreshTokenFromCookies(context);
-//     const baseURL = `${process.env.NEXT_PUBLIC_SERVER_API}`;
-
-//     let accessToken = access;
-
-//     if (!accessToken) {
-//       const response = await axios.post(`${baseURL}/account/token/refresh/`, {
-//         refresh,
-//       });
-//       setAccessTokenToCookies(context, response.data.access);
-//       setRefreshTokenToCookies(context, response.data.refresh);
-//       accessToken = response.data.access;
-//     }
-
-//     const axiosInstance = axios.create({
-//       baseURL,
-//       headers: { Authorization: `Bearer ${accessToken}` },
-//     });
-
-//     axiosInstance.interceptors.request.use(async (req: InternalAxiosRequestConfig) => {
-//       const access_decoded: { exp: number } = jwtDecode(accessToken as string);
-//       const isExpired = access_decoded.exp * 1000 < new Date().getTime();
-
-//       if (isExpired) {
-//         const response = await axios.post(`${baseURL}/account/token/refresh/`, {
-//           refresh,
-//         });
-//         setAccessTokenToCookies(context, response.data.access);
-//         setRefreshTokenToCookies(context, response.data.refresh);
-
-//         req.headers.Authorization = `Bearer ${response.data.access as string}`;
-//         accessToken = response.data.access;
-//         return req;
-//       }
-
-//       if (!refresh) {
-//         if (typeof window !== "undefined") {
-//           window.location.replace(
-//             process.env.NODE_ENV === "development"
-//               ? "http://dev-fe.standalone.incento.kr/auth/login"
-//               : "https://dev-fe.standalone.incento.kr/auth/login"
-//           );
-//         }
-//         return req;
-//       }
-
-//       const refresh_decoded: { exp: number } = jwtDecode(refresh as string);
-//       if (refresh_decoded.exp * 1000 < new Date().getTime()) {
-//         deleteCookie("access_standalone", {
-//           ...context,
-//         });
-//         deleteCookie("refresh_standalone", {
-//           ...context,
-//         });
-//         if (typeof window !== "undefined") {
-//           window.location.replace(
-//             process.env.NODE_ENV === "development"
-//               ? "http://dev-fe.standalone.incento.kr/auth/login"
-//               : "https://dev-fe.standalone.incento.kr/auth/login"
-//           );
-//         }
-//         return req;
-//       }
-
-//       return req;
-//     });
-
-//     axiosInstance.interceptors.response.use((response) => {
-//       return response;
-//     });
-
-//     return axiosInstance;
-//   } catch (err) {
-//     if (typeof window !== "undefined") {
-//       window.location.replace(
-//         process.env.NODE_ENV === "development"
-//           ? "http://dev-fe.standalone.incento.kr/auth/login"
-//           : "https://dev-fe.standalone.incento.kr/auth/login"
-//       );
-//     }
-//     return;
-//   }
-// };
