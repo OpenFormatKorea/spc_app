8325beda1b05738adea36a1c62bcb0b9d5cc3979
brianj-incento <heuiyeong@incento.kr>
2025-03-18 10:53:14 +0900

    tailwind fix
    tailwind fix

--- src/interfaces/tailwindCss.ts
+++ src/interfaces/tailwindCss.ts
@@ -1,4 +1,4 @@
 export const theadStyle =
-  "px-[15px] py-[9px] border-b border-gray-200 text-left text-[13px] font-medium text-gray-700 text-center";
+  "px-[20px] py-[15px] border-b border-gray-200 text-left text-[15px] font-semibold text-gray-700 text-center";
 export const tbodyStyle =
   "px-[9px] py-[5px] border-b border-gray-200 whitespace-normal break-words break-all text-[13px] text-center items-center";
